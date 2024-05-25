3f255369f62699bda47dcb689e3a43663de5c5cd
PaperChalice <liujunchang97@outlook.com>
2024-05-25 19:00:52 +0800

    don't fail for gcc-*

--- Library/Homebrew/cmd/deps.rb
+++ Library/Homebrew/cmd/deps.rb
@@ -326,7 +326,7 @@ module Homebrew
           is_circular = dep_stack.include?(dep.name)
           if is_circular
             display_s = "#{display_s} (CIRCULAR DEPENDENCY)"
-            Homebrew.failed = true
+            Homebrew.failed = !dep.name.include?("gcc")
           end
 
           puts "#{prefix}#{display_s}"
